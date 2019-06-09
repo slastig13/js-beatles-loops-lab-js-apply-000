579f1269dd2dbd8bda2a4e7d7e4feb53c9ec2327
Samantha Lastig <slastig13@gmail.com>
2019-06-09 14:25:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  let array = [];
+
+  for (let i = 0; i < musicians.length; i++) {
+    let person = musicians[i];
+
+    for (let j = 0; j < instruments.length; j++) {
+      let instrument = instruments[j];
+
+      let element = `${person} plays ${instrument}`;
+      array.push(element);
+    }
+  }
+  return array;
+}
